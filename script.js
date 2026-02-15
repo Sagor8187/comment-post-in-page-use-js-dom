91027e8fa69d5fb1c15fced3d176307b385d5027
Sagor8187 <sdsagor8187@gmail.com>
2026-02-15 20:45:32 +0600

    complete my layout and functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+let sections = document.getElementById("postmain")
+let posts = document.getElementById("inputtext")
+let btn = document.getElementById("post-btn")
+// console.log(sections,posts,btn)
+
+btn.addEventListener("click",()=>{
+    let data = posts.value
+    
+    let newpost = document.createElement("p")
+    newpost.classList.add("text")
+    newpost.innerText = data
+    sections.appendChild(newpost)
+    posts.value = ""
+})
